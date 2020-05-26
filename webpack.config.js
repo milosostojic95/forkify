73dcf5b9a3370ca20cc6c0cf765835905e588584
Milos Ostojic <ostoja_95_cacak@hotmail.com>
2020-05-26 11:04:27 +0200

    Instaled web server and html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,21 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: require.resolve('./src/js/index.js'),
   context: __dirname,
   mode: 'development',
   output: {
-    path: path.resolve(__dirname, 'dist/js'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
-  }
+  },
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins:[
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html'
+    })
+  ]
 }
