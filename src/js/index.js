4a7ac57889dee929fb85330626897041d1d16afc
Milos Ostojic <ostoja_95_cacak@hotmail.com>
2020-05-27 11:30:44 +0200

    Added search controller

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,27 @@
 import '../sass/main.scss';
 import Search from './modules/Search';
 
-const search = new Search('pizza');
-console.log(search);
+const state = {};
 
-search.getResults();
+const controlSearch = async () => {
+  // 1. get quert from view
+  const query = 'pizza'
+  if(query) {
+    //2. new search object
+    state.search = new Search(query);
+    //3. prepare Ui for result
+
+    //4. search for result
+    await state.search.getResults();
+    //5. render result on Ui
+    console.log(state.search.result)
+  }
+}
+
+
+document.querySelector('.search-form').addEventListener('submit',(e)=> {
+  e.preventDefault();
+  controlSearch();
+})
+
+console.log(state)
